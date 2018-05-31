916831f0402dfd51421b0d734f76168cfdeb9209
silvester <silvester>
2018-05-31 15:26:19 +0200

    Write the jasmine test for the limit feature.

--- spec/toolsSpec.js
+++ spec/toolsSpec.js
@@ -26,7 +26,8 @@ describe("Sorted arrays", function() {
     expect(sortedFalse).toBe(tools.sortArray(sortedFalse, "name", false));
   });
 });
-describe("Filtered array", function() {
+
+describe("Filtered arrays", function() {
 	var Tools = require('../src/Tools');
 	let arrayToFilter = [];
 	let filtered = [];
@@ -47,8 +48,39 @@ describe("Filtered array", function() {
     expect(filtered.length).toBe(0);
   });
 
-  it("should not be larger", function() {
+  it("should not be larger than the original", function() {
 	 filtered = tools.filterArray(arrayToFilter, String.fromCharCode(65 + Math.floor(Math.random() * 25)));
     expect(filtered.length).not.toBeGreaterThan(arrayToFilter.length);
+  });
+});
+
+describe("Limited arrays", function() {
+	var Tools = require('../src/Tools');
+	let arrayToLimit = [];
+	let limitedUp = [];
+	let limitedDown = [];
+	let limit = 0;
+
+	beforeEach(function() {
+	  tools = new Tools();
+		while (arrayToLimit.length < 10) {
+			arrayToLimit.push({
+				balance: Math.floor(Math.random() * 100)
+			});
+		}
+	});
+
+  it("should be empty if all elements are off limit", function() {
+	 limitedUp = tools.limitArray(arrayToLimit, -200, 1);
+	 limitedDown = tools.limitArray(arrayToLimit, 200, -1);
+	 expect(limitedUp.length).toBe(0);
+    expect(limitedDown.length).toBe(0);
+  });
+
+  it("should be complementary", function() {
+		limit = Math.floor(Math.random() * 50);
+	 limitedUp = tools.limitArray(arrayToLimit, limit, 1);
+	 limitedDown = tools.limitArray(arrayToLimit, limit, -1);
+    expect(limitedUp.length + limitedDown.length).toBe(arrayToLimit.length);
   });
 });
